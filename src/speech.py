df7f196fdfe8b1c9511098c582935e397ca06a94
glebite <glebite@gmail.com>
2022-03-18 09:58:07 -0400

    Adding speech output for now.

---
+++ src/speech.py
@@ -0,0 +1,30 @@
+"""
+speech.py - the 
+
+"""
+import platform
+
+
+class Speaker:
+    """
+    """
+    def __init__(self):
+        """__init__ - constructor for the Speaker class
+        """
+        self.platform = platform.system()
+        if self.platform == 'Windows':
+            import win32com
+            self.speaker = win32com.client.Dispatch('SAPI.SpVoice')
+
+    def _winspeak(self, message):
+        """
+        """
+        self.speaker.Speak(message)
+        
+    def speak(self, message):
+        """speak - use the os type to "say" something
+        """
+
+
+if __name__ == "__main__":
+    pass
