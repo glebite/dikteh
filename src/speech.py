f0ed5a756f44297abdd589fd7ac9c7817bfe2b10
glebite <glebite@gmail.com>
2022-03-18 10:04:40 -0400

    Fixed merge problem.

--- src/speech.py
+++ src/speech.py
@@ -1,19 +1,14 @@
 """
-<<<<<<< HEAD
 speech.py - the 
 
 """
 import platform
-=======
-"""
->>>>>>> 7549c05332f34db919f41102209020759a521153
 
 
 class Speaker:
     """
     """
     def __init__(self):
-<<<<<<< HEAD
         """__init__ - constructor for the Speaker class
         """
         self.platform = platform.system()
@@ -29,16 +24,12 @@ class Speaker:
     def speak(self, message):
         """speak - use the os type to "say" something
         """
-=======
-        """
-        """
         pass
 
     def speak(self, message):
         """
         """
         pass
->>>>>>> 7549c05332f34db919f41102209020759a521153
 
 
 if __name__ == "__main__":
