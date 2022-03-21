57fc61515dbbea1bab4c290750a5ff5b691f56ab
glebite <glebite@gmail.com>
2022-03-20 20:09:05 -0400

    Gameplay is okay thus far...

--- src/cli-dikteh.py
+++ src/cli-dikteh.py
@@ -16,6 +16,8 @@ class CLI:
         self.speaker = Speaker()
         self.speaker.configure()
         self.sentences_to_play = 1
+        self.failed_words = list()
+        self.score = {'success': 0, 'missed': 0}
         pd.set_option('display.max_colwidth', None)
 
     def load_sentences(self):
@@ -51,7 +53,18 @@ class CLI:
         """
         return "".join([char for char in sentence
                         if char not in string.punctuation])
-    
+
+    def report_score(self):
+        """
+        """
+        print('Good work with studying!')
+        print(f'Sentences used: {self.sentences_to_play}')
+        print(f'Success: {self.score["success"]}')
+        print(f'Missed : {self.score["missed"]}')
+        print(f'Specifically missed words: ')
+        for word, readword in self.failed_words:
+            print(f'Expected: {word} Typed: {readword}')
+
     def game_play(self):
         """game_play - play the loop
 
@@ -65,15 +78,19 @@ class CLI:
         n/a
         """
         for count in range(0, self.sentences_to_play):
-            for word in game.pick_random_sentence().tolist()[0].split():        
-                word = game.remove_punctuation(word)
+            print(f'Playing sentence {count} of {self.sentences_to_play}.')
+            for word in game.pick_random_sentence().tolist()[0].split():     
+                word = game.remove_punctuation(word).lower()
                 game.speaker.speak(word)
                 readword = input('Enter the word that you heard: ')
                 if readword != word:
                     print(f'You typed: {readword}  and the word spoken was: {word}')
+                    self.score['missed'] += 1
+                    self.failed_words.append((word, readword))
                 else:
-                    print(f'Correct!')            
-
+                    self.score['success'] += 1
+                    print(f'Correct!')       
+        self.report_score()
 
 if __name__ == "__main__":
     game = CLI(sys.argv[1])
