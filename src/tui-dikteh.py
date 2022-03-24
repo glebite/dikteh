323b26b06725cfda149bd33d06a5084d0d9d72b8
glebite <glebite@gmail.com>
2022-03-24 19:28:47 -0400

    PUtting together the tui version.

---
+++ src/tui-dikteh.py
@@ -0,0 +1,18 @@
+"""tui-dikteh.py
+"""
+import sys
+import pandas as pd
+import string
+from speech import Speaker
+import curses
+
+
+class TUI:
+    """TUI 
+    """
+    def __init__(self, wordfile, sentence_count):
+        pass
+
+
+if __name__ == "__main__":
+    pass
