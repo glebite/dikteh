ddfff676cbf458cecb4dac7d4484539d490fc6d0
glebite <glebite@gmail.com>
2022-03-29 20:02:19 -0400

    Adding in label, stats information.

--- src/tui_dikteh.py
+++ src/tui_dikteh.py
@@ -6,6 +6,7 @@ import string
 from speech import Speaker
 import curses
 from cli_dikteh import CLI
+import time
 
 
 class TUI(CLI):
@@ -14,34 +15,46 @@ class TUI(CLI):
     def __init__(self, wordfile, sentence_count):
         super(TUI, self).__init__(wordfile , sentence_count)
         self.stdscr = None
-        pass
 
     def start_display(self):
         """start_display - instantiate the screen member
         """
         self.stdscr = curses.initscr()
+        if not self.stdscr:
+            print("Problem opening the window...")
+        curses.noecho()
+        curses.curs_set(0)
 
     def stop_display(self):
         """stop_display - shut down the display for now
         """
+        curses.curs_set(1)
         self.stdscr.endwin()
 
+    def label(self, row, column, message):
+        """
+        """
+        self.stdscr.addstr(row, column, message)
+        self.stdscr.refresh()
+
+    def display_stats(self):
+        """
+        """
+        pass
+        
     def __del__(self):
         """__del__ - handle the destruction of the object
         """
         self.stop_display()
 
     """ interaction code """
-
-    def display_message(self, coord_pair, message):
-        """
-        """
-        pass
     
     def game_play(self):
         """
         """
-        pass
+        self.start_display()
+        time.sleep(10)
+        self.stop_display()
     
     def accept_input(self):
         """
@@ -63,4 +76,3 @@ if __name__ == "__main__":
     x = TUI(sys.argv[1], sys.argv[2])
     x.load_sentences()
     x.game_play()
-
